161bf67f2e0e2aa4fdae9e803d0f5a5f8bab12d4
TransshipmentEnvoy <TransshipmentEnvoy@outlook.com>
2025-11-24 22:38:24 +0800

    new: asan & ubsan

---
+++ src/libsim_estab/cmake/sanitizer.cmake
@@ -0,0 +1,34 @@
+include_guard()
+
+function(target_enable_asan TARGET)
+    if (MSVC)
+        message(STATUS "${TARGET}: AddressSanitizer not enabled on MSVC")
+        return()
+    endif()
+
+    message(STATUS "${TARGET}: enabling AddressSanitizer")
+    target_compile_options(${TARGET} PRIVATE
+        -fsanitize=address
+        -fno-omit-frame-pointer
+        -fno-optimize-sibling-calls
+    )
+    target_link_options(${TARGET} PRIVATE
+        -fsanitize=address
+    )
+endfunction()
+
+function(target_enable_ubsan TARGET)
+    if (MSVC)
+        message(STATUS "${TARGET}: UndefinedBehaviorSanitizer not enabled on MSVC")
+        return()
+    endif()
+
+    message(STATUS "${TARGET}: enabling UndefinedBehaviorSanitizer")
+    target_compile_options(${TARGET} PRIVATE
+        -fsanitize=undefined
+        -fno-omit-frame-pointer
+    )
+    target_link_options(${TARGET} PRIVATE
+        -fsanitize=undefined
+    )
+endfunction()
